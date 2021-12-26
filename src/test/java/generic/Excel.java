926147f6cfad7b42143831863b196f319f536ec9
hp <hp@DESKTOP-QI4JHSQ>
2021-12-26 22:55:07 +0530

    Updated TestNG Grid as yes

--- src/test/java/generic/Excel.java
+++ src/test/java/generic/Excel.java
@@ -9,11 +9,17 @@ public class Excel {
 	public static String getData(String path,String sheet,int r,int c) {
 		String v="";
 			try {
+				System.out.println(path);
+				System.out.println(sheet);
+				System.out.println(r);
+				System.out.println(c);
 				Workbook wb = WorkbookFactory.create(new FileInputStream(path));
 				v = wb.getSheet(sheet).getRow(r).getCell(c).toString();
 				wb.close();
+				System.out.println("Exceldata" +v); 
 			}
 			catch (Exception e) {
+				e.printStackTrace();
 			}
 		return v;
 	}
@@ -26,6 +32,7 @@ public class Excel {
 				wb.close();
 			}
 			catch (Exception e) {
+				
 			}
 		return v;
 	}
